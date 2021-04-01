607e0d534f83933035032e0dcf0f32791cab4d2c
LuK1337 <priv.luk@gmail.com>
2021-04-01 13:27:49 +0200

    kebab: Import sensors/2.0/multihal
    Change-Id: I2615438912974f2f0f57fd96b930d4bd11d0de5b
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -460,7 +460,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.oneplus_kona \
     libsensorndkbridge
 
 # Soong namespaces
