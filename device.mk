a8a52b67f2ca67e50aabe70f151769264fa9269f
LibXZR <i@xzr.moe>
2021-09-01 18:22:22 +0800

    kebab: Implement kscope charge control HAL
    [chandu078]: Address chgctrl denials
    * avc: denied { write } for name="mmi_charging_enable" dev="sysfs" ino=82999 scontext=u:r:hal_chargecontrol_default:s0 tcontext=u:object_r:vendor_sysfs_usb_supply:s0 tclass=file permissive=0
    
    * avc: denied { search } for name="oplus_chg" dev="sysfs" ino=82898 scontext=u:r:hal_chargecontrol_default:s0 tcontext=u:object_r:vendor_sysfs_usb_supply:s0 tclass=dir permissive=0
    
    Signed-off-by: LibXZR <i@xzr.moe>
    Change-Id: I7441eebc675a957a10138d5c448fc45c872883d0
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -126,6 +126,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.kscope.chgctrl@1.0-service.oplus_kona
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
