313a9d1f5b17df28c944aa25e3400de3c2c9b8ae
Bruno Martins <bgcngm@gmail.com>
2022-04-12 12:34:23 +0100

    kebab: device: Drop QTI WiFi interfaces
    Change-Id: I91f4042868bbcd18cc16862821f7dc831cb1ca65
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -522,11 +522,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
-    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
-    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
