soumya19x/android_device_oneplus_lemonkebab

kebab: Add init script to set properties per variant
Co-Authored-by: Lucchetto <lucchetto.tie@live.com>
Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

BoardConfig.mk
@@ -133,6 +133,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sm8250
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 

chandu078/device_oneplus_kebab

BoardConfig.mk
@@ -135,6 +135,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_kona
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 