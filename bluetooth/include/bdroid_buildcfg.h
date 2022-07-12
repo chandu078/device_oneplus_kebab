76ab6a7629b7d331640512ad7370b11f056bf160
chandu078 <chandudyavanapelli03@gmail.com>
2022-07-12 18:39:31 +0530

    kebab: Add OnePlus 9R support
    - Let's make unified build for both 8T and 9R device, since 8T and 9R are similar device.
    - Update blobs, configs and init scripts from OnePlus 9R LE2101 11_C.33.
    - Add 9R device, name and model props according to PRJ and RF version.
    - Set bluetooth name based on PRJ version.
    - Override usb name for OnePlus 9R device.
    - Add system property runtime resource overlay for 9R power profile, default settings global device name and default hotspot ssid for both 8T and 9R device.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,7 +22,49 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "OnePlus 8T"
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+#include "osi/include/osi.h"
+
+typedef struct {
+    const char *prj_version;
+    const char *product_model;
+} device_t;
+
+static const device_t devices[] = {
+    {"19805", "OnePlus 8T"},
+    {"20809", "OnePlus 8T"},
+    {"20828", "OnePlus 9R"},
+};
+
+static inline const char *BtmGetDefaultName()
+{
+    char prj_version[92];
+    property_get("ro.boot.prj_version", prj_version, "");
+
+    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
+        device_t device = devices[i];
+
+        if (strcmp(device.prj_version, prj_version) == 0) {
+            return device.product_model;
+        }
+    }
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
