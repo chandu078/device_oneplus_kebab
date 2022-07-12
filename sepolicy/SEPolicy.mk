e19acc58baa93557cdc4ed922314ed76c6746cd7
chandu078 <chandudyavanapelli03@gmail.com>
2022-07-12 17:40:33 +0530

    sm8250-common: Merge hardware/oplus components
    [All credits to related authors on]
    - https://github.com/LineageOS/android_hardware_oplus/commits/lineage-19.1.
    
    [Drop]:
    - HighTouchPollingRate interface
    - SM8350 rules
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+BOARD_VENDOR_SEPOLICY_DIRS += device/oneplus/sm8250-common/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/oneplus/sm8250-common/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/oneplus/sm8250-common/sepolicy/public
