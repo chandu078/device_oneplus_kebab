0e34dc4bdaef288ca4819a6e3d54d36abfae1131
chandu078 <chandudyavanapelli03@gmail.com>
2022-07-29 04:14:12 +0000

    kebab: init: Set device, name & model props according to PRJ and RF version
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- init/init_oplus_kona.cpp
+++ init/init_oplus_kona.cpp
@@ -7,82 +7,124 @@
 #include <android-base/properties.h>
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sysinfo.h>
+#include <sys/system_properties.h>
 #include <sys/_system_properties.h>
 
+#include "property_service.h"
+#include "vendor_init.h"
+
 using android::base::GetProperty;
+using std::string;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "vendor_dlkm."
+};
 
 /*
  * SetProperty does not allow updating read only properties and as a result
- * does not work for our use case. Write "OverrideProperty" to do practically
+ * does not work for our use case. Write "property_override" to do practically
  * the same thing as "SetProperty" without this restriction.
  */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
 
-    prop_info* pi = (prop_info*)__system_property_find(name);
-    if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = true) {
+    string prop_name;
+
+    for (const auto &source : ro_props_default_source_order) {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str());
     }
 }
+
+void vendor_load_properties() {
+
+  string model;
+  string device;
+  string name;
 
 /*
  * Only for read-only properties. Properties that can be wrote to more
  * than once should be set in a typical init script (e.g. init.oplus.hw.rc)
  * after the original property has been set.
  */
-void vendor_load_properties() {
-    auto device = GetProperty("ro.product.product.device", "");
-    auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
 
-    switch (rf_version) {
-        case 11: // CN
-            if (device == "OnePlus8") {
-                OverrideProperty("ro.product.product.model", "IN2010");
-            } else if (device == "OnePlus8T") {
-                OverrideProperty("ro.product.product.model", "KB2000");
-            } else if (device == "OnePlus8Pro") {
-                OverrideProperty("ro.product.product.model", "IN2020");
-            }
-            break;
-        case 12: // TMO
-            if (device == "OnePlus8") {
-                OverrideProperty("ro.product.product.model", "IN2017");
-            } else if (device == "OnePlus8T") {
-                OverrideProperty("ro.product.product.model", "KB2007");
-            } else if (device == "OnePlus8Pro") {
-                OverrideProperty("ro.product.product.model", "IN2027");
-            }
-            break;
-        case 13: // IN
-            if (device == "OnePlus8") {
-                OverrideProperty("ro.product.product.model", "IN2011");
-            } else if (device == "OnePlus8T") {
-                OverrideProperty("ro.product.product.model", "KB2001");
-            } else if (device == "OnePlus8Pro") {
-                OverrideProperty("ro.product.product.model", "IN2021");
-            }
-            break;
-        case 14: // EU
-            if (device == "OnePlus8") {
-                OverrideProperty("ro.product.product.model", "IN2013");
-            } else if (device == "OnePlus8T") {
-                OverrideProperty("ro.product.product.model", "KB2003");
-            } else if (device == "OnePlus8Pro") {
-                OverrideProperty("ro.product.product.model", "IN2023");
-            }
-            break;
-        case 15: // NA
-            if (device == "OnePlus8") {
-                OverrideProperty("ro.product.product.model", "IN2015");
-            } else if (device == "OnePlus8T") {
-                OverrideProperty("ro.product.product.model", "KB2005");
-            } else if (device == "OnePlus8Pro") {
-                OverrideProperty("ro.product.product.model", "IN2025");
-            }
-            break;
+  auto prj_version = std::stoi(GetProperty("ro.boot.prj_version", "0"));
+  auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
+
+  switch(prj_version){
+       /* OnePlus 8T */
+    case 19805:
+          device = "OnePlus8T";
+      switch (rf_version){
+           /* China */
+        case 11:
+          name = "OnePlus8T_CN";
+          model = "KB2000";
+          break;
+            /* India */
+        case 13:
+          name = "OnePlus8T_IN";
+          model = "KB2001";
+          break;
+            /* Europe */
+        case 14:
+          name = "OnePlus8T_EU";
+          model = "KB2003";
+          break;
+            /* Global / US Unlocked */
+        case 15:
+          name = "OnePlus8T_NA";
+          model = "KB2005";
+          break;
+            /* Generic */
         default:
-            LOG(ERROR) << "Unexpected RF version: " << rf_version;
-    }
+          name = "OnePlus8T_NA";
+          model = "KB2005";
+          break;
+      }
+      break;
+       /* OnePlus 8T T-Mobile */
+    case 20809:
+          device = "OnePlus8T";
+      switch (rf_version){
+            /* T-Mobile */
+        case 12:
+          name = "OnePlus8T_TMO";
+          model = "KB2007";
+          break;
+            /* Generic */
+        default:
+          name = "OnePlus8T_NA";
+          model = "KB2005";
+          break;
+      }
+      break;
+  }
+
+    set_ro_build_prop("device", device);
+    set_ro_build_prop("model", model);
+    set_ro_build_prop("name", name);
+    set_ro_build_prop("product", model, false);
 }
